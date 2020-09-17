e7719adc9787dc14c1db5c42298e39d056413cf3
Phellipe Sander <phellipe.sander1@gmail.com>
2020-09-16 21:52:10 -0300

    adicionando componente header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-      <h1>Hello Semana DevSuperior!</h1>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
